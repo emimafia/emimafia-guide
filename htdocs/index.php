934e7ad4574acacb6453dc67e6dc6427c61228be
Christoph Kepler <christoph.kepler@arts.aero>
2013-11-06 09:35:06 +0100

    Layout Concept

--- htdocs/index.php
+++ htdocs/index.php
@@ -16,26 +16,94 @@
   <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
   <link rel="shortcut icon" href="/favicon.ico" />
   <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/style.css" media="screen">
+  <!-- Scripts -->
   <script type="text/javascript" src="lib/html5shiv.js"></script>
   <script type="text/javascript" src="lib/jquery-2.0.3.min.js"></script>
   <script type="text/javascript" src="scripts/js/ajax.js"></script>
 </head>
 
 <body>
-  <div>
+  <div id="wrapper">
     <header>
-      <h1>Under Construction</h1>
+      <h1>Hier findest du demnächst eine Karte</h1>
     </header>
-    <nav>
-      <p><a href="/">Home</a></p>
-    </nav>
-
-    <div>
-      <p>
-      	Hier wird demnächst sowas unendlich Geiles enstehen. Ihr werdet nicht an euch halten können.
-      </p>
+    <div id='content'>
+    	<nav>
+    		<p>Navigation</p>
+    		<ul>
+    			<li>
+    				<a href="/">Home</a>
+    			</li>
+    			<li>
+    				<a href="#">PoI 1</a>
+    			</li>
+    			<li>
+    				<a href="#">PoI 2</a>
+    			</li>
+    			<li>
+    				<a href="#">PoI 3</a>
+    			</li>
+    			<li>
+    				<a href="#">PoI 4</a>
+    			</li>
+    			<li>
+    				<a href="#">PoI 5</a>
+    			</li>
+    		</ul>
+    	</nav>
+      <article>
+       Hier soll dann mal der Beschreibungtext rein<br />
+       Lorem Ipsum<br />
+       Dolor Et<br />
+       Bart Bart Bart Bart<br />
+       Gandalf-/Weberstyle<br />
+      </article>
+      <div id="audio">
+      	Playa
+      </div>
+      <div id="video">
+      	Youtube
+      </div>
+      <div class="clearfix"></div>
+      <div id="gallery">
+      	<div class="thumbnail">
+      		Hier
+      	</div>
+      	<div class="thumbnail">
+      		könnte
+      	</div>
+      	<div class="thumbnail">
+      		man
+      	</div>
+      	<div class="thumbnail">
+      		via
+      	</div>
+      	<div class="thumbnail">
+      		PHP-Schleife
+      	</div>
+      	<div class="thumbnail">
+      		ne
+      	</div>
+      	<div class="thumbnail">
+      		Bildergalerie
+      	</div>
+      	<div class="thumbnail">
+      		mit
+      	</div>
+      	<div class="thumbnail">
+      		Thumbnails
+      	</div>
+      	<div class="thumbnail">
+      		anzeigen
+      	</div>
+      	<div class="thumbnail">
+      		lassen.
+      	</div>
+      </div>
+      <div class="clearfix"></div>
     </div>
-
     <footer>
      <p>&copy; Copyright  by Christoph Kepler, Michael Schneider, Malte Pasche, Lukas Landgraf</p>
     </footer>
