fa48acaed96e25f6cafe6569149cc96d77f356ed
Christoph Kepler <christoph.kepler@arts.aero>
2014-01-06 10:26:45 +0100

    Gallery should work now
    Configuring of flexslider
    Implementation of lightbox

--- htdocs/scripts/js/general.js
+++ htdocs/scripts/js/general.js
@@ -4,16 +4,23 @@
  */
 
 $(window).load(function() {
-  $('.flexslider').flexslider();
-  $(".thumb").click(
-    function(){  
-       $("#show_original").html('<img class="close" src="'+this.src.replace('tn_','')+'"  /> ');
-      $("#grey, #show_original").fadeIn(300);  
-    }
-  );  
-  $(".close").click(
-    function(){  
-      $("#grey, #show_original").fadeOut(300);  
-    }
-  );  
+  $('.flexslider').flexslider( {
+		animation : "slide",
+		animationLoop : true,
+		itemWidth : 130,
+		minItems : 2,
+		maxItems : 7,
+		controlsContainer : '.flex-container' 
+  });
+  // $(".full").click(
+    // function(){  
+       // $("#show_original").html('<img class="close" src="'+this.src.replace('tn_','')+'"  /> ');
+      // $("#grey, #show_original").fadeIn(300);  
+    // }
+  // );  
+  // $(".close").click(
+    // function(){  
+      // $("#grey, #show_original").fadeOut(300);  
+    // }
+  // );
 });
