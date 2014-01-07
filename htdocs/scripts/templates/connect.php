b02cc946b20ecaa07a894e53d5386d5fdb09b1e0
emimafia <emimafia@users.noreply.github.com>
2014-01-07 16:40:35 +0100

    Changed Password

--- htdocs/scripts/templates/connect.php
+++ htdocs/scripts/templates/connect.php
@@ -1,7 +1,7 @@
 <?php
 	$server = 'localhost';
 	$user = 'emimafia_guide';
-	$pw = 'EMI-Dre$den4';
+	$pw = 'DAS GEHT EUCH NIX AN';
 	$db = 'emimafia_guide';
 	mysql_connect($server, $user, $pw) or die ("Keine Verbindung möglich!");
 	mysql_select_db($db) or die ("Die Datenbank ist nicht erreichbar!");
