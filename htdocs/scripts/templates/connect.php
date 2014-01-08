1bbe27d6d6b5908fa54fa8812175c13e3abd4cfe
Christoph Kepler <christoph.kepler@arts.aero>
2014-01-08 08:29:57 +0100

    Trying

--- htdocs/scripts/templates/connect.php
+++ htdocs/scripts/templates/connect.php
@@ -1,7 +1,7 @@
 <?php
 	$server = 'localhost';
 	$user = 'emimafia_guide';
-	$pw = 'DAS GEHT EUCH NIX AN';
+	$pw = 'DAS GEHT EUCH NIX AN! NICHT MAL ANNÄHERND';
 	$db = 'emimafia_guide';
 	mysql_connect($server, $user, $pw) or die ("Keine Verbindung möglich!");
 	mysql_select_db($db) or die ("Die Datenbank ist nicht erreichbar!");
