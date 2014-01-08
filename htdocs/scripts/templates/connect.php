533e4af460878c7eb4e5828b548c9b7420c09b1e
Christoph Kepler <christoph.kepler@arts.aero>
2014-01-08 09:47:25 +0100

    Code Cleaning

--- htdocs/scripts/templates/connect.php
+++ htdocs/scripts/templates/connect.php
@@ -4,5 +4,5 @@
 	$pw = 'DAS GEHT EUCH NIX AN! NICHT MAL ANNÄHERND';
 	$db = 'emimafia_guide';
 	mysql_connect($server, $user, $pw) or die ("Keine Verbindung möglich!");
-	mysql_select_db($db) or die ("Die Datenbank ist nicht erreichbar!");
+	mysql_select_db($db) or die ("Die Datenbank ist nicht erreichbar!");	
 ?>
