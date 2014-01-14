eb5872362e145755a4a522f651fe3dac079e0f86
Christoph Kepler <christoph.kepler@arts.aero>
2014-01-14 13:34:47 +0100

    Fixed Navigation Issue with home and gallery

--- htdocs/index.php
+++ htdocs/index.php
@@ -77,7 +77,7 @@
 	</nav>
   <div id="wrapper">
     <header>
-      <h1>Emimafia Dresden Guide</h1>
+      <a href="scripts/templates/home.php" class="ajax-link"><h1>Emimafia Dresden Guide</h1></a>
       <div id="styleswitch">
 	      <div class="stylebuttons" onclick="styleswitch('plus')">+</div>
 	      <div class="stylebuttons" onclick="styleswitch('minus')">-</div>
@@ -88,120 +88,27 @@
 	      </div>
       </div>
       <div id="map"></div> <!-- Hier wird die Map eingebunden -->
-      <div id="osm">©<a href="http://www.openstreetmap.org">OpenStreetMap</a>
-   		und <a href="http://www.openstreetmap.org/copyright">Mitwirkende</a>,
-   		<a href="http://creativecommons.org/licenses/by-sa/2.0/deed.de">CC-BY-SA</a>
- 	</div>
+      <div id="osm">
+      	©
+      	<a href="http://www.openstreetmap.org">
+      		OpenStreetMap
+      	</a>
+   			und
+   			<a href="http://www.openstreetmap.org/copyright">
+   				Mitwirkende
+   			</a>
+   			, 
+   			<a href="http://creativecommons.org/licenses/by-sa/2.0/deed.de">
+   				CC-BY-SA
+   			</a>
+ 			</div>
     </header>
     <div id="subheader"></div>
     <div id='content'>
-      <div id="homepage-wrapper">
-      	<div id="homepage" class="sw">
-	      	<h2>Der Emimafia Guide</h2>
-	      	<br />
-	      	<p>
-	      		Dresden wird wegen seiner barocken Architektur, berühmten Bauwerken wie der Frauenkirche oder der Semperoper, als auch durch seine Lage das deutsche bzw. Elbflorenz genannt. Aber dies sind nicht alle Sehenswürdigkeiten in der Landeshauptstadt. Bei uns entdecken sie noch weitere Orte und und erleben Dresden aus einem anderen Blickwinkel. 
-	       	</p>
-	       	<br />
-	       	<table cellspacing="0" cellpadding="0" border="0">
-	       		<thead>
-	       			<tr>
-	       				<td colspan="2">
-	       					Ein paar generelle Fakten über Dresden
-	       				</td>
-	       			</tr>
-	       		</thead>
-	       		<tbody>
-		       		<tr class="odd">
-		       			<td>
-		       				Bundesland
-		       			</td>
-		       			<td>
-		       				Sachsen
-		       			</td>
-		       		</tr>
-		       		<tr class="even">
-		       			<td>
-		       				Fläche
-		       			</td>
-		       			<td>
-		       				328,31 km²
-		       			</td>	       			
-		       		</tr>
-		       		<tr class="odd">
-		       			<td>
-		       				Höhe
-		       			</td>
-		       			<td>
-		       				112 m ü. NHN
-		       			</td>	       			
-		       		</tr>
-		       		<tr class="even">
-		       			<td>
-		       				Einwohner
-		       			</td>
-		       			<td>
-		       				525.105
-		       			</td>	       			
-		       		</tr>
-		       		<tr class="odd">
-		       			<td>
-		       				Bevölkerungsdichte
-		       			</td>
-		       			<td>
-		       				1599 Einwohner je km²
-		       			</td>	       			
-		       		</tr>
-		       	</tbody>
-	       	</table>
-	      </div>
-	      <div id="logo">
-	      	<img src="css/images/dresden_stadtwappen.svg" />
-	      </div>
-      </div>
-      <div class="clearfix"></div>
-      <div id="gallery">
-      <div class="flexslider">
-			  	<ul class="slides">
-			  	<?php
-								$dir = 'media/images';
-								if(!is_dir($dir)){
-								mkdir($dir,0755);
-								}
-			  		$tmp = opendir($dir);
-			  		while($file=readdir($tmp)) {
-			  			if($file != '.' && $file != '..') {
-			  				$dir_arr[] .= $file;
-			  			}
-			  		}
-			      closedir($tmp);
-			  		foreach($dir_arr as &$dir_arr_entry) {
-			  			$link = array (
-			  				'frauenkirche' => '1',
-			  				'semperoper' => '2',
-			  				'altmarkt' => '3',
-			  				'grossergarten' => '4',
-			  				'waldschloesschenbruecke' => '5',
-			  			);
-			  			$tmp = opendir($dir . '/' . $dir_arr_entry);
-			  			while($file=readdir($tmp)){
-				        $fname = $dir . '/' . $dir_arr_entry . '/' . $file;
-				        $tnname = $dir . '/' . $dir_arr_entry . '/tn_' . $file; 
-			        	if(is_file($fname) && strpos($fname,'tn_')=== FALSE){
-			          	echo '
-			   		<li>
-			    		<a href="' . $fname . '" data-lightbox="gallery"><img class="thumb" src="' . $tnname . '"  /></a>
-			   		</li>';
-			        	}
-			      	}
-			      	closedir($tmp);
-			  		}
-			      
-			    ?>
-					</ul>
-				</div>
-      </div>
-      <div class="clearfix"></div>
+    	<?php 
+    	$isinclude = TRUE;
+    	include 'scripts/templates/home.php';
+    	?>
     </div>
     <footer>
     <div id="additional-links" class="sw"><a href="static/impressum.html" class="ajax-link">Impressum</a> | <a href="static/contact.php" class="ajax-link">Contact</a></div>
--- htdocs/scripts/templates/home.php
+++ htdocs/scripts/templates/home.php
@@ -1,107 +1,106 @@
 <div id="homepage-wrapper">
-      	<div id="homepage" class="sw">
-	      	<h2>Der Emimafia Guide</h2>
-	      	<br />
-	      	<p>
-	      		Dresden wird wegen seiner barocken Architektur, berühmten Bauwerken wie der Frauenkirche oder der Semperoper, als auch durch seine Lage das deutsche bzw. Elbflorenz genannt. Aber dies sind nicht alle Sehenswürdigkeiten in der Landeshauptstadt. Bei uns entdecken sie noch weitere Orte und und erleben Dresden aus einem anderen Blickwinkel. 
-	       	</p>
-	       	<br />
-	       	<table cellspacing="0" cellpadding="0" border="0">
-	       		<thead>
-	       			<tr>
-	       				<td colspan="2">
-	       					Ein paar generelle Fakten über Dresden
-	       				</td>
-	       			</tr>
-	       		</thead>
-	       		<tbody>
-		       		<tr class="odd">
-		       			<td>
-		       				Bundesland
-		       			</td>
-		       			<td>
-		       				Sachsen
-		       			</td>
-		       		</tr>
-		       		<tr class="even">
-		       			<td>
-		       				Fläche
-		       			</td>
-		       			<td>
-		       				328,31 km²
-		       			</td>	       			
-		       		</tr>
-		       		<tr class="odd">
-		       			<td>
-		       				Höhe
-		       			</td>
-		       			<td>
-		       				112 m ü. NHN
-		       			</td>	       			
-		       		</tr>
-		       		<tr class="even">
-		       			<td>
-		       				Einwohner
-		       			</td>
-		       			<td>
-		       				525.105
-		       			</td>	       			
-		       		</tr>
-		       		<tr class="odd">
-		       			<td>
-		       				Bevölkerungsdichte
-		       			</td>
-		       			<td>
-		       				1599 Einwohner je km²
-		       			</td>	       			
-		       		</tr>
-		       	</tbody>
-	       	</table>
-	      </div>
-	      <div id="logo">
-	      	<img src="css/images/dresden_stadtwappen.svg" />
-	      </div>
-      </div>
-      <div class="clearfix"></div>
-      <div id="gallery">
-      <div class="flexslider">
-			  	<ul class="slides">
-			  	<?php
-								$dir = '../../media/images';
-								if(!is_dir($dir)){
-								mkdir($dir,0755);
-								}
-			  		$tmp = opendir($dir);
-			  		while($file=readdir($tmp)) {
-			  			if($file != '.' && $file != '..') {
-			  				$dir_arr[] .= $file;
-			  			}
-			  		}
-			      closedir($tmp);
-			  		foreach($dir_arr as &$dir_arr_entry) {
-			  			$link = array (
-			  				'frauenkirche' => '1',
-			  				'semperoper' => '2',
-			  				'altmarkt' => '3',
-			  				'grossergarten' => '4',
-			  				'waldschloesschenbruecke' => '5',
-			  			);
-			  			$tmp = opendir($dir . '/' . $dir_arr_entry);
-			  			while($file=readdir($tmp)){
-				        $fname = $dir . '/' . $dir_arr_entry . '/' . $file;
-				        $tnname = $dir . '/' . $dir_arr_entry . '/tn_' . $file; 
-			        	if(is_file($fname) && strpos($fname,'tn_')=== FALSE){
-			          	echo '
-			   		<li>
-			    		<a href="' . $fname . '" data-lightbox="gallery"><img class="thumb" src="' . $tnname . '"  /></a>
-			   		</li>';
-			        	}
-			      	}
-			      	closedir($tmp);
-			  		}
-			      
-			    ?>
-					</ul>
-				</div>
-      </div>
-      <div class="clearfix"></div>
+	<div id="homepage" class="sw">
+	<h2>Der Emimafia Guide</h2>
+	<br />
+	<p>
+		Dresden wird wegen seiner barocken Architektur, berühmten Bauwerken wie der Frauenkirche oder der Semperoper, als auch durch seine Lage das deutsche bzw. Elbflorenz genannt. Aber dies sind nicht alle Sehenswürdigkeiten in der Landeshauptstadt. Bei uns entdecken sie noch weitere Orte und und erleben Dresden aus einem anderen Blickwinkel. 
+	</p>
+	<br />
+	<table cellspacing="0" cellpadding="0" border="0">
+		<thead>
+			<tr>
+				<td colspan="2">
+					Ein paar generelle Fakten über Dresden
+				</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="odd">
+				<td>
+					Bundesland
+				</td>
+				<td>
+					Sachsen
+				</td>
+			</tr>
+			<tr class="even">
+				<td>
+					Fläche
+				</td>
+				<td>
+					328,31 km²
+				</td>	       			
+			</tr>
+			<tr class="odd">
+				<td>
+					Höhe
+				</td>
+				<td>
+					112 m ü. NHN
+				</td>	       			
+			</tr>
+			<tr class="even">
+				<td>
+					Einwohner
+				</td>
+				<td>
+					525.105
+				</td>	       			
+			</tr>
+			<tr class="odd">
+				<td>
+					Bevölkerungsdichte
+				</td>
+				<td>
+					1599 Einwohner je km²
+				</td>	       			
+			</tr>
+		</tbody>
+	</table>
+	</div>
+	<div id="logo">
+		<img src="css/images/dresden_stadtwappen.svg" />
+	</div>
+</div>
+<div class="clearfix"></div>
+<div id="gallery">
+	<div class="flexslider">
+		<ul class="slides">
+			<?php
+				($isinclude === TRUE) ? $dir = 'media/images' : $dir = '../../media/images';
+				if(!is_dir($dir)){
+					mkdir($dir,0755);
+				}
+				$tmp = opendir($dir);
+				while($file=readdir($tmp)) {
+					if($file != '.' && $file != '..') {
+						$dir_arr[] .= $file;
+					}
+				}
+				closedir($tmp);
+				foreach($dir_arr as &$dir_arr_entry) {
+					$link = array (
+						'frauenkirche' => '1',
+						'semperoper' => '2',
+						'altmarkt' => '3',
+						'grossergarten' => '4',
+						'waldschloesschenbruecke' => '5',
+					);
+					$tmp = opendir($dir . '/' . $dir_arr_entry);
+					while($file=readdir($tmp)){
+						$fname = $dir . '/' . $dir_arr_entry . '/' . $file;
+						$tnname = $dir . '/' . $dir_arr_entry . '/tn_' . $file; 
+						if(is_file($fname) && strpos($fname,'tn_')=== FALSE){
+							echo '
+			<li>
+				<a href="' . $fname . '" data-lightbox="gallery"><img class="thumb" src="' . $tnname . '"  /></a>
+			</li>';
+						}
+					}
+					closedir($tmp);
+				}    
+			?>
+		</ul>
+	</div>
+</div>
+<div class="clearfix"></div>
